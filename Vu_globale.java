90ea7c1d78d81cf6efe2dc1228e68b718feddb6f
Fatima <88293932+mo-ony@users.noreply.github.com>
2025-12-24 11:26:28 +0100

    Update Vu_globale.java

--- Vu_globale.java
+++ Vu_globale.java
@@ -1,10 +1,7 @@
-Bonsoir François,
-
-On a implémenté le mécanisme de checksum et, suite à cela, j’ai mis à jour le GAD.
+Afin d’améliorer les mesures d’intégrité de la solution GED, on a implémenté le mécanisme de checksum et, suite à cela, j’ai mis à jour le GAD.
 
 Peux-tu me préciser où je peux déposer / uploader la nouvelle version du GAD ?
 
 Merci d’avance.
 
 Bonne soirée,
-Fatima
